20f35962e90652b49e7cec174fe0068ce783c59c
0marSalah <omar.salah1597@gmail.com>
2023-05-09 18:17:51 +0300

    clearing

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -5,8 +5,3 @@ def print_last_digit(number):
         number *= -1
     print("{}".format(number % 10), end="")
     return ("{}".format(number % 10))
-
-print_last_digit(98)
-print_last_digit(0)
-r = print_last_digit(-1024)
-print(r)
