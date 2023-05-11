5a8dd42431466237c6ff650fe0dd4f27f90231c1
0marSalah <omar.salah1597@gmail.com>
2023-05-11 16:04:54 +0300

    fix int()

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     from sys import argv
-    n = len(argv)
-    sum = 0
-    for i in range(n - 1):
-        sum += argv[i + 1]
-    print("{}".format(sum))
+    total = 0
+    for i in range(len(argv) - 1):
+        total += int(argv[i + 1])
+    print("{}".format(total))
