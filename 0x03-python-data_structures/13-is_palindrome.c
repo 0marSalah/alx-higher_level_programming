c36d7ef39e470a7d798de4611b92d504480514f7
0marSalah <omar.salah1597@gmail.com>
2023-05-14 15:05:40 +0300

    fix matrix

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 /**
- * is_palindrome - checks if a singly linked list is a palindrome
+     * is_palindrome - checks if a singly linked list is a palindrome
  * @head: pointer to head of list
  * Return: 0 if it is not a palindrome, 1 if it is a palindrome
  */
