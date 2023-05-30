2bac795d9c935e27082d2de1f4abdc92fcb8bc71
0marSalah <omar.salah1597@gmail.com>
2023-05-30 19:01:04 +0300

    ya3ammm

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
+
 """This module defines a Square class"""
 
 class Square:
-    """This class implements a Square"""
+    """This class is a square with a size attribute"""
+
     def __init__(self):
+        """Initializes a Square with a size attribute"""
         pass
