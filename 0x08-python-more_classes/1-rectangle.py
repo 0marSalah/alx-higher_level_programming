2b4449ffbf96fdc410c3a91ec4b796819fc61a60
0marSalah <omar.salah1597@gmail.com>
2023-06-05 16:30:49 +0300

    fix it

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -5,10 +5,10 @@
 
 class Rectangle:
     """Defines a Rectangle."""
-    def __init__(self):
+    def __init__(self, width=0, height=0):
         """Initializes a Rectangle."""
-        self.__width = 0
-        self.__height = 0
+        self.__width = width
+        self.__height = height
 
     def width(self):
         """getter for width attribute."""
@@ -25,3 +25,11 @@ class Rectangle:
     def height(self, value):
         """setter for height attribute."""
         self.__height = value
+
+
+my_rectangle = Rectangle(2, 4)
+print(my_rectangle.__dict__)
+
+my_rectangle.width = 10
+my_rectangle.height = 3
+print(my_rectangle.__dict__)
