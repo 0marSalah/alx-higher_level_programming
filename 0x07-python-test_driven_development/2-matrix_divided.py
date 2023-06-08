3ee64f066b1ab36f5738da86e160639794200091
0marSalah <omar.salah1597@gmail.com>
2023-06-08 14:14:25 +0300

    fix 4 + 5 + 2 + 0

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,5 +1,4 @@
-#!usr/bin/python3
-
+#!/usr/bin/python3
 """module for matrix_divided function."""
 
 
--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,22 +1,29 @@
 #!/usr/bin/python3
-
-
-"""
-module contains a function that prints a text with 2 new lines
-after each of these characters: ., ? and :
-"""
+"""Defines a text-indentation function."""
 
 
 def text_indentation(text):
+    """Print text with two new lines after each '.', '?', and ':'.
+
+    Args:
+        text (string): The text to print.
+    Raises:
+        TypeError: If text is not a string.
     """
-    function that prints a text with 2 new lines after each of
-    these characters: ., ? and :
-    """
-    if type(text) is not str:
+    if not isinstance(text, str):
         raise TypeError("text must be a string")
-    for i in range(len(text)):
-        if text[i] == '.' or text[i] == '?' or text[i] == ':':
-            print(text[i])
-            print()
-        else:
-            print(text[i], end="")
+
+    c = 0
+    while c < len(text) and text[c] == ' ':
+        c += 1
+
+    while c < len(text):
+        print(text[c], end="")
+        if text[c] == "\n" or text[c] in ".?:":
+            if text[c] in ".?:":
+                print("\n")
+            c += 1
+            while c < len(text) and text[c] == ' ':
+                c += 1
+            continue
+        c += 1
