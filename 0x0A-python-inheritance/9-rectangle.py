af4279fb4a39a2c943deb39f89c4867f4624d4b0
0marSalah <omar.salah1597@gmail.com>
2023-06-12 14:16:35 +0300

    rect class with area and str

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""this module contains a class Rectangle that inherits from BaseGeometry"""
+
+
+class Rctangle(BaseGeometry):
+    """Represents a Rectangle."""
+
+    def __init__(self, width, height):
+        """
+        Initializes an instance.
+        Args:
+        width: width of the rectangle
+        height: height of the rectangle
+        """
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        """Returns the area of the rectangle."""
+        return self.__width * self.__height
+
+    def __str__(self):
+        """Returns the rectangle description."""
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
