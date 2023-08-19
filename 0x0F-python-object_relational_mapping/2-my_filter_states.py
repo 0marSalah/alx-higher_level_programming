879bbcd985e7de39cc228c311a982123adf9e142
0marSalah <omar.salah1597@gmail.com>
2023-08-19 13:30:51 +0300

    momken tany

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,22 +1,21 @@
 #!/usr/bin/python3
 """
-Lists all states with a name starting
-where name matches the argument.
+Lists all values in the states tables of a database where name
+matches the argument
 """
 import sys
 import MySQLdb
 
 if __name__ == '__main__':
-    user = sys.argv[1]
-    passwd = sys.argv[2]
-    db_name = sys.argv[3]
-    state_name = sys.argv[4]
-    db = MySQLdb.connect(user=user, passwd=passwd, db=db_name, port=3306)
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
+                         db=sys.argv[3], port=3306)
 
     cur = db.cursor()
-    cur.execute("SELECT * FROM states;")
+    cur.execute("SELECT * \
+    FROM states \
+    WHERE CONVERT(`name` USING Latin1) \
+    COLLATE Latin1_General_CS = '{}';".format(sys.argv[4]))
     states = cur.fetchall()
 
     for state in states:
-        if state[1] == state_name:
-            print(state)
+        print(state)
