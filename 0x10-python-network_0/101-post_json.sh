812068aafaf4e2b11e6dce38244efb7567494285
0marSalah <omar.salah1597@gmail.com>
2023-08-31 21:45:11 +0300

    101 second try

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Bash scripts that sends a POST request to a given URL.
-curl -sX -H "Content-Type: application/json" -d "$2" "$1"
+curl -sX POST -H "Content-Type: application/json" -d "$2" "$1"
