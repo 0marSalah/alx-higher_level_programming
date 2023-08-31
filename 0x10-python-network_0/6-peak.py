8d0b7b9ca4cd705105101acdbf956b734edab968
0marSalah <omar.salah1597@gmail.com>
2023-08-31 21:29:55 +0300

    nums

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -2,15 +2,15 @@
 """Finds a peak in a list of unsorted integers"""
 
 
-def find_peak(list_of_integers):
-    """Finds a peak in list_of_integers"""
+def find_peak(nums):
+    """Finds a peak in nums"""
 
-    s, e = 0, len(list_of_integers) - 1
+    s, e = 0, len(nums) - 1
     while s <= e:
         m = s + ((e - s) // 2)
-        if m > 0 and list_of_integers[m] < list_of_integers[m - 1]:
+        if m > 0 and nums[m] < nums[m - 1]:
             e = m - 1
-        elif m < len(list_of_integers) - 1 and list_of_integers[m] < list_of_integers[m + 1]:
+        elif m < len(nums) - 1 and nums[m] < nums[m + 1]:
             s = m + 1
         else:
-            return list_of_integers[m]
+            return nums[m]
