c596af24817f99d9c69059e280b4c759bfb898a5
0marSalah <omar.salah1597@gmail.com>
2023-08-31 18:39:10 +0300

    4

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# get body only if status 200
-echo "$(curl -s -X POST -d "email='test@gmail.com'&subject='I will always be here for PLD'" "$1")"
+# Bash scripts that sends a POST request to a given URL.
+curl -s -X POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
